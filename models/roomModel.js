f33df23fa868f40407162854c2dc2ef307da6d27
Quentin JADOUL <18155@ecam.be>
2021-12-21 13:04:33 +0100

    class room

---
+++ models/roomModel.js
@@ -0,0 +1,9 @@
+class Room {
+    constructor(name,width,){
+        this.name = name;
+        this.width = width;
+        this.length = length;
+        //ajouter surface
+    }
+}
+module.exports = Room;
